aab51d07469549db3c18858f9876be1d107b48f5
M4nuski <the_man994@hotmail.com>
2016-02-19 21:21:47 -0500

    initial code commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace ImageComparer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startCompareButton = new System.Windows.Forms.Button();
+            this.logTextBox = new System.Windows.Forms.TextBox();
+            this.baseFolderBox = new System.Windows.Forms.TextBox();
+            this.compareFolderBox = new System.Windows.Forms.TextBox();
+            this.trashFolderBox = new System.Windows.Forms.TextBox();
+            this.selectBaseFolderButton = new System.Windows.Forms.Button();
+            this.AddCompareFolderButton = new System.Windows.Forms.Button();
+            this.ChangeTrashFolder = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.clearCompareFolderListButton = new System.Windows.Forms.Button();
+            this.searchEmptyFolderButton = new System.Windows.Forms.Button();
+            this.bubbleEmptyPathButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startCompareButton
+            // 
+            this.startCompareButton.Location = new System.Drawing.Point(12, 337);
+            this.startCompareButton.Name = "startCompareButton";
+            this.startCompareButton.Size = new System.Drawing.Size(134, 62);
+            this.startCompareButton.TabIndex = 0;
+            this.startCompareButton.Text = "Scan and Trash Duplicate Images";
+            this.startCompareButton.UseVisualStyleBackColor = true;
+            this.startCompareButton.Click += new System.EventHandler(this.startCompareButton_Click);
+            // 
+            // logTextBox
+            // 
+            this.logTextBox.Location = new System.Drawing.Point(152, 121);
+            this.logTextBox.Margin = new System.Windows.Forms.Padding(3, 6, 3, 3);
+            this.logTextBox.Multiline = true;
+            this.logTextBox.Name = "logTextBox";
+            this.logTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.logTextBox.Size = new System.Drawing.Size(526, 278);
+            this.logTextBox.TabIndex = 1;
+            this.logTextBox.WordWrap = false;
+            // 
+            // baseFolderBox
+            // 
+            this.baseFolderBox.Location = new System.Drawing.Point(152, 11);
+            this.baseFolderBox.Name = "baseFolderBox";
+            this.baseFolderBox.ReadOnly = true;
+            this.baseFolderBox.Size = new System.Drawing.Size(526, 20);
+            this.baseFolderBox.TabIndex = 2;
+            // 
+            // compareFolderBox
+            // 
+            this.compareFolderBox.Location = new System.Drawing.Point(152, 40);
+            this.compareFolderBox.Multiline = true;
+            this.compareFolderBox.Name = "compareFolderBox";
+            this.compareFolderBox.ReadOnly = true;
+            this.compareFolderBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.compareFolderBox.Size = new System.Drawing.Size(526, 72);
+            this.compareFolderBox.TabIndex = 3;
+            // 
+            // trashFolderBox
+            // 
+            this.trashFolderBox.Location = new System.Drawing.Point(153, 407);
+            this.trashFolderBox.Name = "trashFolderBox";
+            this.trashFolderBox.ReadOnly = true;
+            this.trashFolderBox.Size = new System.Drawing.Size(526, 20);
+            this.trashFolderBox.TabIndex = 4;
+            this.trashFolderBox.Text = "C:\\$Recycle.Bin";
+            // 
+            // selectBaseFolderButton
+            // 
+            this.selectBaseFolderButton.Location = new System.Drawing.Point(12, 10);
+            this.selectBaseFolderButton.Name = "selectBaseFolderButton";
+            this.selectBaseFolderButton.Size = new System.Drawing.Size(134, 23);
+            this.selectBaseFolderButton.TabIndex = 5;
+            this.selectBaseFolderButton.Text = "Select Base Folder";
+            this.selectBaseFolderButton.UseVisualStyleBackColor = true;
+            this.selectBaseFolderButton.Click += new System.EventHandler(this.selectBaseFolderButton_Click);
+            // 
+            // AddCompareFolderButton
+            // 
+            this.AddCompareFolderButton.Location = new System.Drawing.Point(12, 39);
+            this.AddCompareFolderButton.Name = "AddCompareFolderButton";
+            this.AddCompareFolderButton.Size = new System.Drawing.Size(134, 23);
+            this.AddCompareFolderButton.TabIndex = 6;
+            this.AddCompareFolderButton.Text = "Add Folder to Compare";
+            this.AddCompareFolderButton.UseVisualStyleBackColor = true;
+            this.AddCompareFolderButton.Click += new System.EventHandler(this.AddCompareFolderButton_Click);
+            // 
+            // ChangeTrashFolder
+            // 
+            this.ChangeTrashFolder.Enabled = false;
+            this.ChangeTrashFolder.Location = new System.Drawing.Point(12, 405);
+            this.ChangeTrashFolder.Name = "ChangeTrashFolder";
+            this.ChangeTrashFolder.Size = new System.Drawing.Size(134, 23);
+            this.ChangeTrashFolder.TabIndex = 7;
+            this.ChangeTrashFolder.Text = "Select Trash Fodler";
+            this.ChangeTrashFolder.UseVisualStyleBackColor = true;
+            this.ChangeTrashFolder.Click += new System.EventHandler(this.ChangeTrashFolder_Click);
+            // 
+            // clearCompareFolderListButton
+            // 
+            this.clearCompareFolderListButton.Location = new System.Drawing.Point(12, 89);
+            this.clearCompareFolderListButton.Name = "clearCompareFolderListButton";
+            this.clearCompareFolderListButton.Size = new System.Drawing.Size(134, 23);
+            this.clearCompareFolderListButton.TabIndex = 8;
+            this.clearCompareFolderListButton.Text = "Clear Folder List";
+            this.clearCompareFolderListButton.UseVisualStyleBackColor = true;
+            this.clearCompareFolderListButton.Click += new System.EventHandler(this.clearCompareFolderListButton_Click);
+            // 
+            // searchEmptyFolderButton
+            // 
+            this.searchEmptyFolderButton.Location = new System.Drawing.Point(12, 121);
+            this.searchEmptyFolderButton.Name = "searchEmptyFolderButton";
+            this.searchEmptyFolderButton.Size = new System.Drawing.Size(133, 62);
+            this.searchEmptyFolderButton.TabIndex = 9;
+            this.searchEmptyFolderButton.Text = "Scan and Trash Empty Folders";
+            this.searchEmptyFolderButton.UseVisualStyleBackColor = true;
+            // 
+            // bubbleEmptyPathButton
+            // 
+            this.bubbleEmptyPathButton.Location = new System.Drawing.Point(11, 189);
+            this.bubbleEmptyPathButton.Name = "bubbleEmptyPathButton";
+            this.bubbleEmptyPathButton.Size = new System.Drawing.Size(134, 62);
+            this.bubbleEmptyPathButton.TabIndex = 10;
+            this.bubbleEmptyPathButton.Text = "Bubble Empty Paths";
+            this.bubbleEmptyPathButton.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(691, 439);
+            this.Controls.Add(this.bubbleEmptyPathButton);
+            this.Controls.Add(this.searchEmptyFolderButton);
+            this.Controls.Add(this.clearCompareFolderListButton);
+            this.Controls.Add(this.ChangeTrashFolder);
+            this.Controls.Add(this.AddCompareFolderButton);
+            this.Controls.Add(this.selectBaseFolderButton);
+            this.Controls.Add(this.trashFolderBox);
+            this.Controls.Add(this.compareFolderBox);
+            this.Controls.Add(this.baseFolderBox);
+            this.Controls.Add(this.logTextBox);
+            this.Controls.Add(this.startCompareButton);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Image Duplicate Finder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startCompareButton;
+        private System.Windows.Forms.TextBox logTextBox;
+        private System.Windows.Forms.TextBox baseFolderBox;
+        private System.Windows.Forms.TextBox compareFolderBox;
+        private System.Windows.Forms.TextBox trashFolderBox;
+        private System.Windows.Forms.Button selectBaseFolderButton;
+        private System.Windows.Forms.Button AddCompareFolderButton;
+        private System.Windows.Forms.Button ChangeTrashFolder;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button clearCompareFolderListButton;
+        private System.Windows.Forms.Button searchEmptyFolderButton;
+        private System.Windows.Forms.Button bubbleEmptyPathButton;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+using ExifLib;
+using Microsoft.VisualBasic.FileIO;
+
+namespace ImageComparer
+{
+    public partial class MainForm : Form
+    {
+        private struct imageInfo
+        {
+            public string fileName;
+            public string fileRelativePath;
+            public string dateTaken;
+            public int width;
+            public int height;
+            public byte[] data;
+        }
+
+        private List<imageInfo> BaseList;
+        private List<List<imageInfo>> CompareLists;
+        private int numDeleted;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void log(string s)
+        {
+            if (logTextBox.Lines.Length > 0) logTextBox.AppendText("\r\n" + s);
+            else logTextBox.Text = s;
+        }
+
+        private List<imageInfo> listFiles(string RootFolder)
+        {
+            return listFiles(RootFolder, RootFolder);
+        }
+        private  List<imageInfo> listFiles(string RootFolder, string CurrentFolder)
+        {
+            var DI = new DirectoryInfo(CurrentFolder);
+            var list = DI.GetFiles("*.jpg").Select(
+                file => LoadImageFileInfo(file.FullName, RootFolder)
+                ).ToList();
+            foreach (var folder in DI.GetDirectories())
+            {
+                list.AddRange(listFiles(RootFolder, folder.FullName));
+            }
+            return list;
+        }
+
+        private void startCompareButton_Click(object sender, EventArgs e)
+        {
+            numDeleted = 0;
+            if (CompareLists.Count > 0)
+            {
+                for (var i = 0; i < CompareLists.Count; i++)
+                {
+                    compareAndDelete(CompareLists[i], compareFolderBox.Lines[i]);
+                }
+                log(numDeleted.ToString("D") + " Files deleted.");
+            } else log("Error: No folders to compare.");
+        }
+
+        private void compareAndDelete(List<imageInfo> listToCompare, string StartFolder)
+        {
+            for (var baseIndex = 0; baseIndex < BaseList.Count; baseIndex++)
+            {
+                for (var compareIndex = 0; compareIndex < listToCompare.Count; compareIndex++)
+                {
+                    //check dates + sizes
+                       //check data
+                          //safeDelete
+                }
+            }
+        }
+
+
+
+        private static void SafeDelete(string fullFileName)
+        {
+            if (File.Exists(fullFileName)) FileSystem.DeleteFile(fullFileName, UIOption.OnlyErrorDialogs, RecycleOption.SendToRecycleBin);
+        }
+
+        private static string removePath(string FullName, string Path)
+        {
+            return FullName.Substring(Path.Length);
+        }
+
+        public byte[] ImageToData(Bitmap data)
+        {
+            var pixelSize = Image.GetPixelFormatSize(data.PixelFormat) / 8;
+            var pixelBufferSize = data.Width * data.Height * pixelSize;
+
+            var pixelSource = new byte[pixelBufferSize];
+
+            var pixData = data.LockBits(new Rectangle(0, 0, data.Width, data.Height), ImageLockMode.ReadOnly, data.PixelFormat);
+            Marshal.Copy(pixData.Scan0, pixelSource, 0, pixelBufferSize);
+            data.UnlockBits(pixData);
+
+            return pixelSource;
+        }
+
+        private imageInfo LoadImageFileInfo(string fullFileName, string RootFolder)
+        {
+            var fi = new FileInfo(fullFileName);
+            try
+            {
+                using (var reader = new ExifReader(fullFileName))
+                {
+
+                    DateTime datePictureTaken;
+                    if (!reader.GetTagValue(ExifTags.DateTimeDigitized, out datePictureTaken))
+                    {
+                        log("Error reading [DateTimeDigitized] tag of: " + fullFileName);
+                        datePictureTaken = fi.LastWriteTime;
+                    }
+
+                    UInt32 pictureWidth;
+                    if (!reader.GetTagValue(ExifTags.PixelXDimension, out pictureWidth))
+                    {
+                        log("Error reading [ImageWidth] tag of: " + fullFileName);
+                    }
+
+                    UInt32 pictureHeight;
+                    if (!reader.GetTagValue(ExifTags.PixelYDimension, out pictureHeight))
+                    {
+                        log("Error reading [ImageLength] tag of: " + fullFileName);
+                    }
+
+                    return new imageInfo
+                    {
+                        fileName = fi.Name,
+                        fileRelativePath = removePath(fi.FullName, RootFolder),
+                        width = (int)pictureWidth,
+                        height = (int)pictureHeight,
+                        dateTaken = datePictureTaken.ToString("yyyy-MM-dd hh-mm-ss"),
+                        data = null
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                log("Error loading exif data in " + fullFileName + ": " + ex.Message);
+
+                using (var bp = new Bitmap(fullFileName)) 
+                return new imageInfo
+                {
+                    fileName = fi.Name,
+                    fileRelativePath = removePath(fi.FullName, RootFolder),
+                    width = bp.Width,
+                    height = bp.Height,
+                    dateTaken = fi.LastWriteTime.ToString("yyyy-MM-dd hh-mm-ss"),
+                    data = null
+                };
+            }
+        }
+
+        private byte[] LoadImageFileData(string fullFileName)
+        {
+            if (File.Exists(fullFileName))
+            {
+                try
+                {
+                    using (var bitmap = new Bitmap(fullFileName))
+                    {
+                        return ImageToData(bitmap);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    log("Error loading image file data of "+fullFileName+" : " + ex.Message);
+                }
+            }
+            return null;
+        }
+
+        private void selectBaseFolderButton_Click(object sender, EventArgs e)
+        {
+            //folderBrowserDialog1.SelectedPath = folderBrowserDialog1.RootFolder.ToString(); infuriating idea...
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                baseFolderBox.Text = folderBrowserDialog1.SelectedPath;
+                BaseList = listFiles(baseFolderBox.Text);
+                log(BaseList.Count.ToString("D") + " Files Loaded.");
+            }
+        }
+
+        private void clearCompareFolderListButton_Click(object sender, EventArgs e)
+        {
+            compareFolderBox.Clear();
+            CompareLists.Clear();
+        }
+
+        private void AddCompareFolderButton_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if (compareFolderBox.Lines.Length > 0) compareFolderBox.AppendText("\r\n");
+                compareFolderBox.AppendText(folderBrowserDialog1.SelectedPath);
+
+                CompareLists.Add(listFiles(folderBrowserDialog1.SelectedPath));
+                log(CompareLists[CompareLists.Count-1].Count.ToString("D") + " Files Loaded.");
+            }
+        }
+
+        private void ChangeTrashFolder_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                trashFolderBox.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+
+    }
+}
